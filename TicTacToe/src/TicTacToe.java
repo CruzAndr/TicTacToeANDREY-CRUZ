c200c1778f058c9772be4b3516cb2fbec984f402
CruzAndr <143221337+CruzAndr@users.noreply.github.com>
2023-10-27 22:22:03 -0600

    Add files via upload

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,10 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Esta es la clase principl que ejecuta el juego
- * @author metal
- */
 public class TicTacToe {
 
     private static final int EN_CURSO = 0;
@@ -18,15 +14,13 @@ public class TicTacToe {
     private GameLogic gameLogic;
     private Player winner;
     private final GameMode gameMode;
-    
-   /**
-    * Este es el constructor de la clase TicTacToe
-    * @param gameMode 
-    */
+    private int totalMoves; // Nuevo atributo para contar los movimientos
+
     public TicTacToe(GameMode gameMode) {
         this.gameMode = gameMode;
         this.board = new Board();
         this.gameStatus = EN_CURSO;
+        this.totalMoves = 0; // Inicializar el contador de movimientos
 
         if (gameMode.getGameMode().equals("Un jugador")) {
             this.player1 = new Player("Jugador 1", gameMode.getPlayerSymbol()
@@ -45,21 +39,18 @@ public class TicTacToe {
 
         this.gameLogic = new GameLogic(player1, player2);
     }
-        /**
-         * metodo que inicia el juego y posee toda su relacon con el usuario
-         */
+
     public void startGame() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bienvenido profe al Tic-Tac-Toe mas insano"
-                + " del mundo");
+        System.out.println("Bienvenido profe al TcTacToe mas insano");
 
-        Player currentPlayer = player1; // Comenzamos con el jugador 1
-        int turnCount = 0;
+        Player currentPlayer = player1;
 
         while (gameStatus == EN_CURSO) {
             board.displayBoard();
             System.out.println("Turno de " + currentPlayer.getName() +
                     " (" + currentPlayer.getSymbol() + ")");
+
             if (currentPlayer == player1 || (currentPlayer == 
                     player2 && ia == null)) {
                 int row = 0, col = 0;
@@ -71,23 +62,23 @@ public class TicTacToe {
                         col = scanner.nextInt();
                     } catch (java.util.InputMismatchException e) {
                         System.out.println("Error: Ingresa numeros validos del"
-                                + "arreglo del gatito.");
-                          System.out.println("Por gracioso lo salto");
+                                + " arreglo del gatito.");
+                        System.out.println("Por gracioso lo salto");
                         scanner.nextLine(); // Limpiar el búfer de entrada
                         continue; // Volver a pedir los valores
                     }
                 } while (!isValidMove(row, col));
 
-                char symbol = currentPlayer.getSymbol(); // Obtén el símbolo del jugador actual
-                gameLogic.makeMove(row, col, symbol); // Llama a makeMove con el símbolo
-                board.placeSymbol(row, col, symbol); // Actualiza el tablero
+                char symbol = currentPlayer.getSymbol();
+                gameLogic.makeMove(row, col, symbol);
+                board.placeSymbol(row, col, symbol);
                 showMoves();
             } else if (currentPlayer == player2 && ia != null) {
                 int[] move = getRandomMove();
                 int row = move[0];
                 int col = move[1];
-                char symbol = currentPlayer.getSymbol(); // Obtén el símbolo del jugador actual
-                board.placeSymbol(row, col, symbol); // Actualiza el tablero
+                char symbol = currentPlayer.getSymbol();
+                board.placeSymbol(row, col, symbol);
                 showMoves();
             }
 
@@ -97,73 +88,67 @@ public class TicTacToe {
                 gameStatus = FIN;
             }
 
-            // Cambiar el turno en función del recuento de turnos
+            if (gameStatus != EN_CURSO) {
+                break;
+            }
+
             if (currentPlayer == player1) {
                 currentPlayer = player2;
             } else {
                 currentPlayer = player1;
             }
 
-            turnCount++; // Aumentar el recuento de turnos
+            totalMoves++;
 
-            // Asegurarse de que el juego no continúe indefinidamente en caso de un error
-            if (turnCount > 9) {
+            if (totalMoves == 9) {
                 System.out.println("Empate");
                 gameStatus = FIN;
             }
         }
 
-       /**
-        * Este codigo demuestra el mensaje del ganador
-        */
         if (gameMode.getGameMode().equals("Un jugador")) {
             if (winner == player2) {
-                System.out.println("La IA te gano, manco.");
+                System.out.println("La IA te gano. MANCO");
             } else if (winner == player1) {
-                System.out.println("El jugador 1 gano, es más bueno que la IA");
+                System.out.println("El jugador 1 gano, es mejor que la IA.");
             } else {
                 System.out.println("Empate");
             }
         } else if (gameMode.getGameMode().equals("Dos jugadores")) {
             if (winner == player1) {
-                System.out.println("El jugador 1 nació para esto.");
+                System.out.println("El jugador 1 nacio para esto.");
             } else if (winner == player2) {
-                System.out.println("El jugador 2 gano.");
+                System.out.println("El jugador 2 gano, increible.");
             } else {
                 System.out.println("Empate.");
             }
         }
     }
-                
+
     private int[] getRandomMove() {
         int row, col;
         Random random = new Random();
 
         do {
-            row = random.nextInt(3); // Genera un número aleatorio entre 0 y 2
-            col = random.nextInt(3); // Genera un número aleatorio entre 0 y 2
-        } while (!isValidMove(row, col)); // Verifica si el movimiento es válido
+            row = random.nextInt(3);
+            col = random.nextInt(3);
+        } while (!isValidMove(row, col));
 
         return new int[]{row, col};
     }
-/**
-                 * metodo que lee las posiciones del arreglo y las devuelve
-                 * @return 
-                 */
+
     private boolean isValidMove(int row, int col) {
-    if (row < 0 || row > 2 || col < 0 || col > 2) {
-        System.out.println("Movimiento no válido. "
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            System.out.println("Movimiento no válido. "
                 + "Ingrese valores entre 0 y 2");
-        return false;
-    } else if (!board.isPositionAvailable(row, col)) {
-        System.out.println("Esa casilla ya está llena, ingrese otra");
-        return false;
+            return false;
+        } else if (!board.isPositionAvailable(row, col)) {
+            System.out.println("Esa casilla ya esta llena, ingrese otra");
+            return false;
+        }
+        return true;
     }
-    return true;
-}
-                    /**
-                     * Metodo que muestra los movimientos en tablero
-                     */
+
     public void showMoves() {
         System.out.println("Movimientos del gatito:");
         for (int i = 0; i < 3; i++) {
@@ -184,10 +169,10 @@ public class TicTacToe {
             try {
                 gameModeChoice = scanner.nextInt();
             } catch (java.util.InputMismatchException e) {
-                System.out.println("Error: Ingresa un numero no una palabra,"
+                System.out.println("Error: Ingresa un numero, no una palabra. "
                         + "POR FAVOR");
-                scanner.nextLine(); // Limpiar el búfer de entrada
-                continue; // Volver a pedir la selección del modo de juego
+                scanner.nextLine();
+                continue;
             }
 
             GameMode gameMode;
@@ -196,19 +181,20 @@ public class TicTacToe {
             } else if (gameModeChoice == 2) {
                 gameMode = new GameMode("Un jugador", 1, "X");
             } else {
-                System.out.println("Opción no válida. Por favor,"
-                        + " selecciona 1 o 2. MANCO");
-                continue; // Volver a pedir la selección del modo de juego
+                System.out.println("Opción no valida. Por favor, "
+                        + "selecciona 1 o 2.");
+                continue;
             }
 
             TicTacToe ticTacToe = new TicTacToe(gameMode);
             ticTacToe.startGame();
 
-            System.out.print("Ya que ganaste, Deseas jugar de nuevo? (S/N): ");
+            System.out.print("Ya que has ganado, "
+                    + "Deseas jugar de nuevo? (S/N): ");
             char playAgain = scanner.next().charAt(0);
 
             if (playAgain != 'S' && playAgain != 's') {
-                System.out.print("Hasta la vista beiby");
+                System.out.print("Hasta la vista.");
                 break;
             }
         } while (true);
